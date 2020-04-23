3619daed180b56cc325ab36de819b158f5b676a2
Ane-Alberdi <60939915+Ane-Alberdi@users.noreply.github.com>
2020-04-23 10:13:42 +0200

    22/04/2020

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,41 @@
+AOS.init({
+    duration:2000
+});
 
+const header = document.querySelector('.header');
 
+window.onscroll = function (){
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 50){
+        header.classList.add('active')
+    }else{
+        header.classList.remove('active');
+    }
 
+}
+const headerikdas = document.querySelector('.headerikdas');
 
+window.onscroll = function (){
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 50){
+        headerikdas.classList.add('active')
+    }else{
+        headerikdas.classList.remove('active');
+    }
 
+}
+
+const headercaixa = document.querySelector('.headercaixa');
+
+window.onscroll = function (){
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 50){
+        headercaixa.classList.add('active')
+    }else{
+        headercaixa.classList.remove('active');
+    }
+
+}
